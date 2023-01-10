edb840d8c28f592a36dcc0d8d4671e15a92d79df
syarosh <sergiy.yarosh@gmail.com>
2023-01-10 11:47:57 +0200

    4th migrate-simple auth + register ok

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 from authentication.forms import LoginForm
+
 
 # Create your views here.
 def login_user(request):
@@ -18,13 +21,26 @@ def login_user(request):
                 context = {
                     'login_form': LoginForm(),
                     'attention': f'The user with username = <{username}> and password were not find '
-                     }
+                }
         else:
             context = {
                 'login_form': LoginForm()
             }
 
     return render(request, 'auth/login.html', context)
+
+
+def register_user(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('index')
+    context = {'form': form}
+    return render(request, 'auth/register.html', context)
 
 
 def logout_user(request):
